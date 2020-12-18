449aa12c3aeaffa0aad1c05a1b693c945f3d1a10
Winfred <1280329846@qq.com>
2020-12-18 17:01:36 +0800

    私家車完成

--- YanFoxconn/app/src/main/java/com/example/administrator/yanfoxconn/constant/Constants.java
+++ YanFoxconn/app/src/main/java/com/example/administrator/yanfoxconn/constant/Constants.java
@@ -8,16 +8,13 @@ package com.example.administrator.yanfoxconn.constant;
 
 public class Constants {
 
-//    public static final String BASE_URL = "http://ytlogisticgps.foxconn.com/SafeServer/";//玲姐
-//    public static final String EMP_BASE = "http://ytlogisticgps.foxconn.com/Server/";    //杰哥
+    public static final String BASE_URL = "http://ytlogisticgps.foxconn.com/SafeServer/";//玲姐10.192.14.19
+    public static final String EMP_BASE = "http://ytlogisticgps.foxconn.com/Server/";    //杰哥
 
-    public static final String BASE_URL = "http://ytsafeserver.foxconn.com/SafeServer/";//玲姐
-    public static final String EMP_BASE = "http://ytsafeserver.foxconn.com/Server/";    //杰哥
-
+   // public static final String BASE_URL = "http://ytsafeserver.foxconn.com/SafeServer/";//玲姐10.192.14.40
+   // public static final String EMP_BASE = "http://ytsafeserver.foxconn.com/Server/";    //杰哥
 
     public static String J4 = "http://192.168.0.108:8080/Server/";//杰哥4F接口測試
-//  public static String J5 = "http://192.168.1.112:8080/Server/";//杰哥5F接口測試
-
     public static String W4 = "http://192.168.1.121:8080/Server/";//我的接口測試
     public static String L4 = "http://192.168.1.137:8080/SafeServer/";//玲姐4F接口測試
 
@@ -58,9 +55,8 @@ public class Constants {
     public static String HTTP_COMMON_FORMS_PHOTOS_SERVLET = EMP_BASE + "CommonFormsupload_photoservlet";//二輪車違規帶圖片上傳
     public static String HTTP_COMMON_FORMS_TWO_WHEEL_SERVLET = EMP_BASE + "CommonFormsTwoWheelServlet";//二輪車違規工號查詢,並獲取門崗列表
     public static String HTTP_COMMON_FORMS_DAY_RECORD_SERVLET = EMP_BASE + "CommonFormsDayRecordServlet";//二輪車違規查詢當日記錄
-    public static String HTTP_COMMON_FORMS_PRIVATE_CAR_SERVLET = W4 + "CommonFormsPrivateCarUpServlet";//私家車違規稽核提交接口
-    public static String HTTP_COMMON_FORMS_PRIVATE_CAR_PARKING = W4 + "CommonFormsPrivateCarParkingServlet";//私家車違規帶出停車場接口
-
+    public static String HTTP_COMMON_FORMS_PRIVATE_CAR_SERVLET = EMP_BASE + "CommonFormsPrivateCarUpServlet";//私家車違規稽核提交接口
+    public static String HTTP_COMMON_FORMS_PRIVATE_CAR_PARKING = EMP_BASE + "CommonFormsPrivateCarParkingServlet";//私家車違規帶出停車場接口
 
     public static String HTTP_COMMON_FORMS_SERVLET = EMP_BASE + "CommonFormsServlet";//工號查詢,並獲取門崗列表
     public static String HTTP_COMMON_GOODS_SERVLET = EMP_BASE + "CommongoodsServlet";//普通物品 工號查詢,並獲取門崗列表，提交信息
